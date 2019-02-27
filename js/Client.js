b08d6eb3a62bf8677d61f7e82aebf1c4eda379b6
AppVeyor <websharper-support@intellifactory.com>
2019-02-27 10:36:49 +0000

    gh-pages.ps1: build -> fsbolero.github.io

---
+++ js/Client.js
@@ -0,0 +1,664 @@
+// $begin{copyright}
+//
+// This file is part of WebSharper
+//
+// Copyright (c) 2008-2018 IntelliFactory
+//
+// Licensed under the Apache License, Version 2.0 (the "License"); you
+// may not use this file except in compliance with the License.  You may
+// obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+// implied.  See the License for the specific language governing
+// permissions and limitations under the License.
+//
+// $end{copyright}
+
+IntelliFactory = {
+    Runtime: {
+        Ctor: function (ctor, typeFunction) {
+            ctor.prototype = typeFunction.prototype;
+            return ctor;
+        },
+
+        Class: function (members, base, statics) {
+            var proto = members;
+            if (base) {
+                proto = new base();
+                for (var m in members) { proto[m] = members[m] }
+            }
+            var typeFunction = function (copyFrom) {
+                if (copyFrom) {
+                    for (var f in copyFrom) { this[f] = copyFrom[f] }
+                }
+            }
+            typeFunction.prototype = proto;
+            if (statics) {
+                for (var f in statics) { typeFunction[f] = statics[f] }
+            }
+            return typeFunction;
+        },
+
+        Clone: function (obj) {
+            var res = {};
+            for (var p in obj) { res[p] = obj[p] }
+            return res;
+        },
+
+        NewObject:
+            function (kv) {
+                var o = {};
+                for (var i = 0; i < kv.length; i++) {
+                    o[kv[i][0]] = kv[i][1];
+                }
+                return o;
+            },
+
+        DeleteEmptyFields:
+            function (obj, fields) {
+                for (var i = 0; i < fields.length; i++) {
+                    var f = fields[i];
+                    if (obj[f] === void (0)) { delete obj[f]; }
+                }
+                return obj;
+            },
+
+        GetOptional:
+            function (value) {
+                return (value === void (0)) ? null : { $: 1, $0: value };
+            },
+
+        SetOptional:
+            function (obj, field, value) {
+                if (value) {
+                    obj[field] = value.$0;
+                } else {
+                    delete obj[field];
+                }
+            },
+
+        SetOrDelete:
+            function (obj, field, value) {
+                if (value === void (0)) {
+                    delete obj[field];
+                } else {
+                    obj[field] = value;
+                }
+            },
+
+        Apply: function (f, obj, args) {
+            return f.apply(obj, args);
+        },
+
+        Bind: function (f, obj) {
+            return function () { return f.apply(this, arguments) };
+        },
+
+        CreateFuncWithArgs: function (f) {
+            return function () { return f(Array.prototype.slice.call(arguments)) };
+        },
+
+        CreateFuncWithOnlyThis: function (f) {
+            return function () { return f(this) };
+        },
+
+        CreateFuncWithThis: function (f) {
+            return function () { return f(this).apply(null, arguments) };
+        },
+
+        CreateFuncWithThisArgs: function (f) {
+            return function () { return f(this)(Array.prototype.slice.call(arguments)) };
+        },
+
+        CreateFuncWithRest: function (length, f) {
+            return function () { return f(Array.prototype.slice.call(arguments, 0, length).concat([Array.prototype.slice.call(arguments, length)])) };
+        },
+
+        CreateFuncWithArgsRest: function (length, f) {
+            return function () { return f([Array.prototype.slice.call(arguments, 0, length), Array.prototype.slice.call(arguments, length)]) };
+        },
+
+        BindDelegate: function (func, obj) {
+            var res = func.bind(obj);
+            res.$Func = func;
+            res.$Target = obj;
+            return res;
+        },
+
+        CreateDelegate: function (invokes) {
+            if (invokes.length == 0) return null;
+            if (invokes.length == 1) return invokes[0];
+            var del = function () {
+                var res;
+                for (var i = 0; i < invokes.length; i++) {
+                    res = invokes[i].apply(null, arguments);
+                }
+                return res;
+            };
+            del.$Invokes = invokes;
+            return del;
+        },
+
+        CombineDelegates: function (dels) {
+            var invokes = [];
+            for (var i = 0; i < dels.length; i++) {
+                var del = dels[i];
+                if (del) {
+                    if ("$Invokes" in del)
+                        invokes = invokes.concat(del.$Invokes);
+                    else
+                        invokes.push(del);
+                }
+            }
+            return IntelliFactory.Runtime.CreateDelegate(invokes);
+        },
+
+        DelegateEqual: function (d1, d2) {
+            if (d1 === d2) return true;
+            if (d1 == null || d2 == null) return false;
+            var i1 = d1.$Invokes || [d1];
+            var i2 = d2.$Invokes || [d2];
+            if (i1.length != i2.length) return false;
+            for (var i = 0; i < i1.length; i++) {
+                var e1 = i1[i];
+                var e2 = i2[i];
+                if (!(e1 === e2 || ("$Func" in e1 && "$Func" in e2 && e1.$Func === e2.$Func && e1.$Target == e2.$Target)))
+                    return false;
+            }
+            return true;
+        },
+
+        ThisFunc: function (d) {
+            return function () {
+                var args = Array.prototype.slice.call(arguments);
+                args.unshift(this);
+                return d.apply(null, args);
+            };
+        },
+
+        ThisFuncOut: function (f) {
+            return function () {
+                var args = Array.prototype.slice.call(arguments);
+                return f.apply(args.shift(), args);
+            };
+        },
+
+        ParamsFunc: function (length, d) {
+            return function () {
+                var args = Array.prototype.slice.call(arguments);
+                return d.apply(null, args.slice(0, length).concat([args.slice(length)]));
+            };
+        },
+
+        ParamsFuncOut: function (length, f) {
+            return function () {
+                var args = Array.prototype.slice.call(arguments);
+                return f.apply(null, args.slice(0, length).concat(args[length]));
+            };
+        },
+
+        ThisParamsFunc: function (length, d) {
+            return function () {
+                var args = Array.prototype.slice.call(arguments);
+                args.unshift(this);
+                return d.apply(null, args.slice(0, length + 1).concat([args.slice(length + 1)]));
+            };
+        },
+
+        ThisParamsFuncOut: function (length, f) {
+            return function () {
+                var args = Array.prototype.slice.call(arguments);
+                return f.apply(args.shift(), args.slice(0, length).concat(args[length]));
+            };
+        },
+
+        Curried: function (f, n, args) {
+            args = args || [];
+            return function (a) {
+                var allArgs = args.concat([a === void (0) ? null : a]);
+                if (n == 1)
+                    return f.apply(null, allArgs);
+                if (n == 2)
+                    return function (a) { return f.apply(null, allArgs.concat([a === void (0) ? null : a])); }
+                return IntelliFactory.Runtime.Curried(f, n - 1, allArgs);
+            }
+        },
+
+        Curried2: function (f) {
+            return function (a) { return function (b) { return f(a, b); } }
+        },
+
+        Curried3: function (f) {
+            return function (a) { return function (b) { return function (c) { return f(a, b, c); } } }
+        },
+
+        UnionByType: function (types, value, optional) {
+            var vt = typeof value;
+            for (var i = 0; i < types.length; i++) {
+                var t = types[i];
+                if (typeof t == "number") {
+                    if (Array.isArray(value) && (t == 0 || value.length == t)) {
+                        return { $: i, $0: value };
+                    }
+                } else {
+                    if (t == vt) {
+                        return { $: i, $0: value };
+                    }
+                }
+            }
+            if (!optional) {
+                throw new Error("Type not expected for creating Choice value.");
+            }
+        },
+
+        ScriptBasePath: "./",
+
+        ScriptPath: function (a, f) {
+            return this.ScriptBasePath + (this.ScriptSkipAssemblyDir ? "" : a + "/") + f;
+        },
+
+        OnLoad:
+            function (f) {
+                if (!("load" in this)) {
+                    this.load = [];
+                }
+                this.load.push(f);
+            },
+
+        Start:
+            function () {
+                function run(c) {
+                    for (var i = 0; i < c.length; i++) {
+                        c[i]();
+                    }
+                }
+                if ("load" in this) {
+                    run(this.load);
+                    this.load = [];
+                }
+            },
+    }
+}
+
+IntelliFactory.Runtime.OnLoad(function () {
+    if (self.WebSharper && WebSharper.Activator && WebSharper.Activator.Activate)
+        WebSharper.Activator.Activate()
+});
+
+// Polyfill
+
+if (!Date.now) {
+    Date.now = function () {
+        return new Date().getTime();
+    };
+}
+
+if (!Math.trunc) {
+    Math.trunc = function (x) {
+        return x < 0 ? Math.ceil(x) : Math.floor(x);
+    }
+}
+
+if (!Object.setPrototypeOf) {
+  Object.setPrototypeOf = function (obj, proto) {
+    obj.__proto__ = proto;
+    return obj;
+  }
+}
+
+function ignore() { };
+function id(x) { return x };
+function fst(x) { return x[0] };
+function snd(x) { return x[1] };
+function trd(x) { return x[2] };
+
+if (!console) {
+    console = {
+        count: ignore,
+        dir: ignore,
+        error: ignore,
+        group: ignore,
+        groupEnd: ignore,
+        info: ignore,
+        log: ignore,
+        profile: ignore,
+        profileEnd: ignore,
+        time: ignore,
+        timeEnd: ignore,
+        trace: ignore,
+        warn: ignore
+    }
+}
+;
+(function()
+{
+ "use strict";
+ var Global,Website,Client,WebSharper,Obj,UI,Var,Operators,View,Arrays,EventTarget,Node,Highlight,SC$1,Snap,Concurrency,WindowOrWorkerGlobalScope,Var$1,ConcreteVar,Scheduler,Abbrev,Fresh,SC$2,CT,CancellationTokenSource,SC$3,IntelliFactory,Runtime,hljs,console,Date;
+ Global=self;
+ Website=Global.Website=Global.Website||{};
+ Client=Website.Client=Website.Client||{};
+ WebSharper=Global.WebSharper=Global.WebSharper||{};
+ Obj=WebSharper.Obj=WebSharper.Obj||{};
+ UI=WebSharper.UI=WebSharper.UI||{};
+ Var=UI.Var=UI.Var||{};
+ Operators=WebSharper.Operators=WebSharper.Operators||{};
+ View=UI.View=UI.View||{};
+ Arrays=WebSharper.Arrays=WebSharper.Arrays||{};
+ EventTarget=Global.EventTarget;
+ Node=Global.Node;
+ Highlight=Website.Highlight=Website.Highlight||{};
+ SC$1=Global.StartupCode$Client$Client=Global.StartupCode$Client$Client||{};
+ Snap=UI.Snap=UI.Snap||{};
+ Concurrency=WebSharper.Concurrency=WebSharper.Concurrency||{};
+ WindowOrWorkerGlobalScope=Global.WindowOrWorkerGlobalScope;
+ Var$1=UI.Var$1=UI.Var$1||{};
+ ConcreteVar=UI.ConcreteVar=UI.ConcreteVar||{};
+ Scheduler=Concurrency.Scheduler=Concurrency.Scheduler||{};
+ Abbrev=UI.Abbrev=UI.Abbrev||{};
+ Fresh=Abbrev.Fresh=Abbrev.Fresh||{};
+ SC$2=Global.StartupCode$WebSharper_Main$Concurrency=Global.StartupCode$WebSharper_Main$Concurrency||{};
+ CT=Concurrency.CT=Concurrency.CT||{};
+ CancellationTokenSource=WebSharper.CancellationTokenSource=WebSharper.CancellationTokenSource||{};
+ SC$3=Global.StartupCode$WebSharper_UI$Abbrev=Global.StartupCode$WebSharper_UI$Abbrev||{};
+ IntelliFactory=Global.IntelliFactory;
+ Runtime=IntelliFactory&&IntelliFactory.Runtime;
+ hljs=Global.hljs;
+ console=Global.console;
+ Date=Global.Date;
+ Client.ToggleDrawer=function()
+ {
+  Client.DrawerShown().Update(function(v)
+  {
+   return!v;
+  });
+ };
+ Client.Main=function()
+ {
+  View.Sink(function(shown)
+  {
+   function a(node,a$1,a$2,a$3)
+   {
+    var o,o$1;
+    (shown?(o=node.classList,function(a$4)
+    {
+     o.add(a$4);
+    }):(o$1=node.classList,function(a$4)
+    {
+     o$1.remove(a$4);
+    }))("shown");
+   }
+   self.document.querySelectorAll(".drawer-backdrop, .lhs-drawer").forEach(Runtime.CreateFuncWithArgs(function($1)
+   {
+    return a($1[0],$1[1],$1[2],$1[3]);
+   }),void 0);
+  },Client.DrawerShown().get_View());
+  Highlight.Run();
+ };
+ Client.DrawerShown=function()
+ {
+  SC$1.$cctor();
+  return SC$1.DrawerShown;
+ };
+ Obj=WebSharper.Obj=Runtime.Class({},null,Obj);
+ Obj.New=Runtime.Ctor(function()
+ {
+ },Obj);
+ Var=UI.Var=Runtime.Class({},Obj,Var);
+ Var.New=Runtime.Ctor(function()
+ {
+  Obj.New.call(this);
+ },Var);
+ Operators.FailWith=function(msg)
+ {
+  throw new Global.Error(msg);
+ };
+ View.Sink=function(act,a)
+ {
+  function loop()
+  {
+   Snap.WhenRun(a(),act,function()
+   {
+    Concurrency.scheduler().Fork(loop);
+   });
+  }
+  Concurrency.scheduler().Fork(loop);
+ };
+ Arrays.get=function(arr,n)
+ {
+  Arrays.checkBounds(arr,n);
+  return arr[n];
+ };
+ Arrays.length=function(arr)
+ {
+  return arr.dims===2?arr.length*arr.length:arr.length;
+ };
+ Arrays.checkBounds=function(arr,n)
+ {
+  if(n<0||n>=arr.length)
+   Operators.FailWith("Index was outside the bounds of the array.");
+ };
+ Highlight.Run=function()
+ {
+  function a(node,a$1,a$2,a$3)
+  {
+   hljs.highlightBlock(node);
+  }
+  self.document.querySelectorAll("code[class^=language-]").forEach(Runtime.CreateFuncWithArgs(function($1)
+  {
+   return a($1[0],$1[1],$1[2],$1[3]);
+  }),void 0);
+ };
+ SC$1.$cctor=function()
+ {
+  SC$1.$cctor=Global.ignore;
+  SC$1.DrawerShown=Var$1.Create$1(false);
+ };
+ Snap.WhenRun=function(snap,avail,obs)
+ {
+  var m;
+  m=snap.s;
+  m==null?obs():m!=null&&m.$==2?(m.$1.push(obs),avail(m.$0)):m!=null&&m.$==3?(m.$0.push(avail),m.$1.push(obs)):avail(m.$0);
+ };
+ Concurrency.scheduler=function()
+ {
+  SC$2.$cctor();
+  return SC$2.scheduler;
+ };
+ Concurrency.Return=function(x)
+ {
+  return function(c)
+  {
+   c.k({
+    $:0,
+    $0:x
+   });
+  };
+ };
+ Var$1=UI.Var$1=Runtime.Class({},Obj,Var$1);
+ Var$1.Create$1=function(v)
+ {
+  return new ConcreteVar.New(false,Snap.New({
+   $:2,
+   $0:v,
+   $1:[]
+  }),v);
+ };
+ Snap.Obsolete=function(sn)
+ {
+  var $1,m,i,$2,o;
+  m=sn.s;
+  if(m==null||(m!=null&&m.$==2?($1=m.$1,false):m!=null&&m.$==3?($1=m.$1,false):true))
+   void 0;
+  else
+   {
+    sn.s=null;
+    for(i=0,$2=Arrays.length($1)-1;i<=$2;i++){
+     o=Arrays.get($1,i);
+     typeof o=="object"?function(sn$1)
+     {
+      Snap.Obsolete(sn$1);
+     }(o):o();
+    }
+   }
+ };
+ Snap.New=function(State)
+ {
+  return{
+   s:State
+  };
+ };
+ ConcreteVar=UI.ConcreteVar=Runtime.Class({
+  Update:function(f)
+  {
+   this.Set(f(this.Get()));
+  },
+  get_View:function()
+  {
+   return this.view;
+  },
+  Set:function(v)
+  {
+   if(this.isConst)
+    (function($1)
+    {
+     return $1("WebSharper.UI: invalid attempt to change value of a Var after calling SetFinal");
+    }(function(s)
+    {
+     console.log(s);
+    }));
+   else
+    {
+     Snap.Obsolete(this.snap);
+     this.current=v;
+     this.snap=Snap.New({
+      $:2,
+      $0:v,
+      $1:[]
+     });
+    }
+  },
+  Get:function()
+  {
+   return this.current;
+  }
+ },Var,ConcreteVar);
+ ConcreteVar.New=Runtime.Ctor(function(isConst,initSnap,initValue)
+ {
+  var $this;
+  $this=this;
+  Var.New.call(this);
+  this.isConst=isConst;
+  this.current=initValue;
+  this.snap=initSnap;
+  this.view=function()
+  {
+   return $this.snap;
+  };
+  this.id=Fresh.Int();
+ },ConcreteVar);
+ Scheduler=Concurrency.Scheduler=Runtime.Class({
+  Fork:function(action)
+  {
+   var $this;
+   $this=this;
+   this.robin.push(action);
+   this.idle?(this.idle=false,Global.setTimeout(function()
+   {
+    $this.tick();
+   },0)):void 0;
+  },
+  tick:function()
+  {
+   var loop,$this,t;
+   $this=this;
+   t=Date.now();
+   loop=true;
+   while(loop)
+    if(this.robin.length===0)
+     {
+      this.idle=true;
+      loop=false;
+     }
+    else
+     {
+      (this.robin.shift())();
+      Date.now()-t>40?(Global.setTimeout(function()
+      {
+       $this.tick();
+      },0),loop=false):void 0;
+     }
+  }
+ },Obj,Scheduler);
+ Scheduler.New=Runtime.Ctor(function()
+ {
+  Obj.New.call(this);
+  this.idle=true;
+  this.robin=[];
+ },Scheduler);
+ Fresh.Int=function()
+ {
+  Fresh.set_counter(Fresh.counter()+1);
+  return Fresh.counter();
+ };
+ Fresh.set_counter=function($1)
+ {
+  SC$3.$cctor();
+  SC$3.counter=$1;
+ };
+ Fresh.counter=function()
+ {
+  SC$3.$cctor();
+  return SC$3.counter;
+ };
+ SC$2.$cctor=function()
+ {
+  SC$2.$cctor=Global.ignore;
+  SC$2.noneCT=CT.New(false,[]);
+  SC$2.scheduler=new Scheduler.New();
+  SC$2.defCTS=[new CancellationTokenSource.New()];
+  SC$2.Zero=Concurrency.Return();
+  SC$2.GetCT=function(c)
+  {
+   c.k({
+    $:0,
+    $0:c.ct
+   });
+  };
+ };
+ View=UI.View=Runtime.Class({},null,View);
+ CT.New=function(IsCancellationRequested,Registrations)
+ {
+  return{
+   c:IsCancellationRequested,
+   r:Registrations
+  };
+ };
+ CancellationTokenSource=WebSharper.CancellationTokenSource=Runtime.Class({},Obj,CancellationTokenSource);
+ CancellationTokenSource.New=Runtime.Ctor(function()
+ {
+  Obj.New.call(this);
+  this.c=false;
+  this.pending=null;
+  this.r=[];
+  this.init=1;
+ },CancellationTokenSource);
+ SC$3.$cctor=function()
+ {
+  SC$3.$cctor=Global.ignore;
+  SC$3.counter=0;
+ };
+ Runtime.OnLoad(function()
+ {
+  Client.Main();
+ });
+}());
+
+
+if (typeof IntelliFactory !=='undefined') {
+  IntelliFactory.Runtime.ScriptBasePath = '/Content/';
+  IntelliFactory.Runtime.Start();
+}
